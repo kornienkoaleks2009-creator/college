38d1d9f8a1b27d54207a902f8a1f04f461645862
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:33:48 +0300

    Commit

--- dynamic array11.cpp
+++ dynamic array11.cpp
@@ -1,8 +1,21 @@
-int count(int value) {
+#include <cstddef>
+
+class Vector {
+private:
+    int* data;
+    size_t size;
+    size_t capacity;
+
+public:
+    int count(int value) const;
+};
+
+int Vector::count(int value) const
+{
     int cnt = 0;
-    for (size_t i = 0; i < size; i++) {
+    for (size_t i = 0; i < size; ++i) {
         if (data[i] == value) {
-            cnt++;
+            ++cnt;
         }
     }
     return cnt;
