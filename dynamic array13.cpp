73ddfb0a74983623ad77eba66e4a6e33819cbbbc
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:14:10 +0300

    Commit

---
+++ dynamic array13.cpp
@@ -0,0 +1,13 @@
+void unique() {
+    if (size <= 1) return;
+    
+    size_t new_size = 1;
+    for (size_t i = 1; i < size; i++) {
+        if (data[i] != data[new_size - 1]) {
+            data[new_size] = data[i];
+            new_size++;
+        }
+    }
+    
+    size = new_size;
+}
