6160c15b612e4d4da2bf84a92691a3651318a5dc
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:36:21 +0300

    Commit

--- dynamic array15.cpp
+++ dynamic array15.cpp
@@ -1,6 +1,14 @@
-void print_string() {
-    for (int i = 0; i < size; i++) {
-        std::cout << data[i];
-    }
-    std::cout << std::endl;
+#include <iostream>
+
+struct char_vector {
+    char* data;
+    int size;
+    int capacity;
+
+    void print_string() const;
+};
+
+void char_vector::print_string() const
+{
+    std::cout.write(data, size);
 }
