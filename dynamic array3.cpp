5f63f69c70ab1f05521922f153c9daa3780c2990
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:31:34 +0300

    Commit

--- dynamic array3.cpp
+++ dynamic array3.cpp
@@ -1,3 +1,16 @@
-bool is_empty() {
+#include <cstddef>
+
+class Vector {
+private:
+    int* data;
+    size_t size;
+    size_t capacity;
+
+public:
+    bool is_empty() const;
+};
+
+bool Vector::is_empty() const
+{
     return size == 0;
 }
