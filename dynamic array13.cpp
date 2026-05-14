c1c4e0b7a94dc55abc1594bc17f3ab89d18ff0b6
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:35:12 +0300

    Commit

--- dynamic array13.cpp
+++ dynamic array13.cpp
@@ -1,13 +1,28 @@
-void unique() {
-    if (size <= 1) return;
-    
+#include <cstddef>
+
+class Vector {
+private:
+    int* data;
+    size_t size;
+    size_t capacity;
+
+public:
+    void unique();
+};
+
+void Vector::unique()
+{
+    if (size <= 1) {
+        return;
+    }
+
     size_t new_size = 1;
-    for (size_t i = 1; i < size; i++) {
+    for (size_t i = 1; i < size; ++i) {
         if (data[i] != data[new_size - 1]) {
             data[new_size] = data[i];
-            new_size++;
+            ++new_size;
         }
     }
-    
+
     size = new_size;
 }
