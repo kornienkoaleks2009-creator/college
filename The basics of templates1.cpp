72139683d1015679fea2ab366305fa874060ec4a
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:39:23 +0300

    Commit

--- The basics of templates1.cpp
+++ The basics of templates1.cpp
@@ -1,4 +1,18 @@
+#include <iostream>
+
 template<typename T>
-T my_min(T a, T b) {
-    return (a < b) ? a : b;
+T my_min(T a, T b)
+{
+    if (a < b)
+        return a;
+    return b;
+}
+
+int main()
+{
+    std::cout << my_min(5, 3) << '\n'; // 3
+    std::cout << my_min(2.5, 8.1) << '\n'; // 2.5
+    std::cout << my_min('a', 'z') << '\n'; // a
+
+    return 0;
 }
