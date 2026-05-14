42283a354bbcd2259ebc0b387d8148c0e0a14a35
Олександр Корнієнко <kornienkoaleks2009@gmail.com>
2026-05-14 15:32:47 +0300

    Commit

--- dynamic array1.cpp
+++ dynamic array1.cpp
@@ -1,5 +1,18 @@
-void pop_back() {
+#include <cstddef>
+
+class Vector {
+private:
+    int* data;
+    size_t size;
+    size_t capacity;
+
+public:
+    void pop_back();
+};
+
+void Vector::pop_back()
+{
     if (size > 0) {
-        size--;
+        --size;
     }
 }
